b1ce25a72757b5d988691414cb8d4cca919b5383
Abigail Watson <awatson1978@gmail.com>
2013-09-15 16:33:40 -0400

    initial commit

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+    summary: "Meteorite package that provides a simple history of the Dow Jones Industrial Average over 5 years."
+});
+
+Package.on_use(function (api) {
+    api.use('standard-app-packages');
+    api.add_files('browser-detection.js', ["client","server"]);
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+1.  You shouldn't use browser-detection.
+http://www.quirksmode.org/js/support.html
+
+2.  But you're going to anyhow, arent't you?
+http://www.quirksmode.org/js/detect.html
