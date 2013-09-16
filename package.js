127fc74221705bdbe40f0fd77f491bb1760aed95
Abigail Watson <awatson1978@gmail.com>
2013-09-15 23:01:11 -0400

    v1.0.4

--- package.js
+++ package.js
@@ -6,5 +6,5 @@ Package.on_use(function (api) {
     api.export('BrowserDetect');
 
     api.use('standard-app-packages');
-    api.add_files('browser-detection.js', ["client","server"]);
+    api.add_files('browser-detection.js', ["client"]);
 });
